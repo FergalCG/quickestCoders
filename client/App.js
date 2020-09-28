52cf4e06d21f1cc2a25feb1daa968569a668312c
FergalCG <fergalcg@gmail.com>
2020-09-27 23:51:44 -0400

    webpack and babel initial setup.

---
+++ client/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+
+
+// class App extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             lesson: ["H", "e", "l", "l", "o", ",", " ", "W", "o", "r", "l", "d", "."]
+//         }
+//     }
+
+//     render() {
+//         return(
+//             <div id='text-input'>
+//                 {this.state.lesson.map( char => <span class='text-item'>{char}</span>)}
+//             </div>
+//         )
+//     }
+// }
+
+const App = () => {
+    return (
+        <div id='text-input'>
+        </div>
+    )
+}
+
+export default App
