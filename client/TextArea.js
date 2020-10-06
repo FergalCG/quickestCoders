0769027da1d9b2a2a5653827d9271fd62d88ec12
FergalCG <fergalcg@gmail.com>
2020-10-05 21:14:05 -0400

    Trying to fix the cursor logic and showing complete and garbage characters.

--- client/TextArea.js
+++ client/TextArea.js
@@ -6,15 +6,20 @@ class TextArea extends Component {
         super()
         this.state = {
             lesson: ["H", "e", "l", "l", "o", ",", " ", "W", "o", "r", "l", "d", "."],
+            status = ['blinker'],
             cursorIdx: 0
         }
     }
 
+    keyPress(event) {
+        console.log(event)
+    }
+
     render() {
-        let { lesson, cursorIdx } = this.state
+        let { lesson, cursorIdx, status } = this.state
         return(
-            <div className='text-container'>
-                {lesson.map( (char, idx) => <span className={cursorIdx === idx ? 'text-item-blink' : 'text-item'} key={idx}>{char}</span>)}
+            <div className={cursorIdx === idx ? 'text-container-blink' : 'text-container'} onClick={() => this.setState({cursorIdx: 0})} onKeyDownCapture={() => this.keyPress}>
+                {lesson.map( (char, idx) => <span className={cursorIdx === idx ? 'text-item-blink': 'text-item'} key={idx}>{char}</span>)}
             </div>
         )
     }
